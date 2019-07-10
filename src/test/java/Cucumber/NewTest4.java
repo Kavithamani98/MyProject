ee96274af643f524e36b404e7fd739366c708a0c
Training_C2A.04.30 <Training_C2A.04.30@CDC2-D-519QS70.dir.svc.accenture.com>
2019-07-10 13:55:56 +0530

    Commit Successfully

--- src/test/java/Cucumber/NewTest4.java
+++ src/test/java/Cucumber/NewTest4.java
@@ -6,6 +6,5 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="C:\\Users\\k.s.senthilkumar\\Cucumber\\src\\resources\\java\\Files.feature")
-
 public class NewTest4 {
 }
